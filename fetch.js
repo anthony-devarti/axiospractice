f46a8b1fc09b23d419f841d02d1075dd10c595ae
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-02-28 23:44:44 +0000

    notes added

--- fetch.js
+++ fetch.js
@@ -18,14 +18,20 @@ const todos = axios.get('https://jsonplaceholder.typicode.com/todos')
 
 //pop the last 10 items in the todos array and push them to the end of the endList
 //need to get the push to wait until the todos promise is fulfilled
+
+//function noted as async right at the top
 async function getList() {
+  //explains what the function is waiting for
     let response = await axios.get('https://jsonplaceholder.typicode.com/todos')
     console.log(response);
+    //the for loop I want to run when we're done waiting
     for (let i=response.data.length-10; i<response.data.length; i++) {
             endList.push(response.data[i])        
     }
     console.log(endList);
+    //makes a myList variable and ids the mylist tag in the html to send this to
     const myList = document.querySelector('#myList');
+    //for each item in the array, run this anonymous function
     endList.forEach(function (item) {
       
       //create a list item
